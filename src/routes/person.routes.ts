d408e064fcb691b435a3422cd81f6785ce0bdd93
Andre.Lucyo2 <andre.lucyo2@gmail.com>
2025-08-19 23:00:29 -0300

    atualizado rotas de person

--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -1,11 +1,65 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { PersonController } from '../controllers/person.controller';
 import { authMiddleware } from '../middleware/auth.middleware';
 
 export const personRoutes = Router();
 
-personRoutes.post('/create', authMiddleware, PersonController.create);
-personRoutes.get('/list', authMiddleware, PersonController.list);
-personRoutes.get('/:uid', authMiddleware, PersonController.getByUid);
-personRoutes.put('/:uid', authMiddleware, PersonController.update);
-personRoutes.delete('/:uid', authMiddleware, PersonController.delete);
+personRoutes.post(
+    '/create',
+    authMiddleware,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await PersonController.create(req, res);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+personRoutes.get(
+    '/list',
+    authMiddleware,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await PersonController.list(req, res);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+personRoutes.get(
+    '/:uid',
+    authMiddleware,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await PersonController.getByUid(req, res);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+personRoutes.put(
+    '/:uid',
+    authMiddleware,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await PersonController.update(req, res);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+personRoutes.delete(
+    '/:uid',
+    authMiddleware,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await PersonController.delete(req, res);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
