a21d4f36765e2013b49bffe060bf41796ba44221
Andre.Lucyo2 <andre.lucyo2@gmail.com>
2025-08-24 22:17:37 -0300

    feat(routs):  rota users para user no singular

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,5 +6,5 @@ import { userRouter } from './user.routes';
 export function setupRoutes(app: Express): void {
 	app.use('/api/v1/auth', authRouter);
 	app.use('/api/v1/person', personRoutes);
-	app.use('/api/v1/users', userRouter);
+	app.use('/api/v1/user', userRouter);
 }
