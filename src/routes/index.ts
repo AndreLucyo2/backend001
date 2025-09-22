d00fd581ecca8098127de88827ba90bad2e4a4a5
Andre.Lucyo2 <andre.lucyo2@gmail.com>
2025-09-21 22:07:21 -0300

    fix:(address) correção de rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express';
 import { authRouter } from './auth.routes';
-import { personRoutes } from './person.routes';
 import { userRouter } from './user.routes';
+import { personRoutes } from './person.routes';
+import { addressRoutes } from './address.routes';
 
 export function setupRoutes(app: Express): void {
 	app.use('/api/v1/auth', authRouter);
-	app.use('/api/v1/person', personRoutes);
 	app.use('/api/v1/user', userRouter);
+	app.use('/api/v1/person', personRoutes);
+	app.use('/api/v1/addresses', addressRoutes);
 }
